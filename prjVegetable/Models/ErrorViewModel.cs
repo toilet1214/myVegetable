edd7bdd62b45c7fc7b540bacec736a3829f990e1
CY <ichang89611@gmail.com>
2024-12-20 14:03:35 +0800

    加入專案檔案。

---
+++ prjVegetable/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace prjVegetable.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
