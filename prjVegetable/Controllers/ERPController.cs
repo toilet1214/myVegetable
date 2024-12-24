f0b2bfd6c8ec126e8873ffa92c005ccdc422c728
Su <swimming.free.fish@gmail.com>
2024-12-24 13:54:28 +0800

    ERP側邊欄

---
+++ prjVegetable/Controllers/ERPController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjVegetable.Controllers
+{
+    public class ERPController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
