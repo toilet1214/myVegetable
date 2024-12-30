6e84e64b9a641ad65fb4b5aac6c90599a44964a7
BZ <ghost.chou.1021@gmail.com>
2024-12-30 09:34:16 +0800

    抓取shiuan分支裡的ERP系統

---
+++ prjVegetable/Controllers/ERPController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjVegetable.Controllers
+{
+    public class ERPController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
