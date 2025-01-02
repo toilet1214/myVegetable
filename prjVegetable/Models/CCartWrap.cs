d6419cd8ee8eee716830c7436b787ba0557c9c4d
CY <ichang89611@gmail.com>
2025-01-02 09:59:55 +0800

    新增wrap

---
+++ prjVegetable/Models/CCartWrap.cs
@@ -0,0 +1,64 @@
+﻿namespace prjVegetable.Models
+{
+    public class CCartWrap
+    {
+        private TCart _cart = null;
+        public TCart Cart
+        {
+            get { return _cart; }
+            set { _cart = value; }
+        }
+        public CCartWrap()
+        {
+            _cart = new TCart();
+        }
+        public int FId
+        { 
+            get { return _cart.FId; } 
+            set { _cart.FId = value; } 
+        }
+
+        public int FProductId 
+        { 
+            get { return _cart.FProductId; } 
+            set { _cart.FProductId = value; } 
+        }
+
+        public string FProductName 
+        { 
+            get { return _cart.FProductName; } 
+            set { _cart.FProductName = value; } 
+        }
+
+        public int FCount 
+        { 
+            get { return _cart.FCount; } 
+            set { _cart.FCount = value; } 
+        }
+
+        public int FPrice 
+        { 
+            get { return _cart.FPrice; } 
+            set { _cart.FPrice = value; } 
+        }
+
+        public int FImgId
+        { 
+            get { return _cart.FImgId; } 
+            set { _cart.FImgId = value; } 
+        }
+
+        public int FBuyerId
+        {
+            get { return _cart.FBuyerId; }
+            set { _cart.FBuyerId = value; }
+        }
+        public int TotalPrice
+        {
+            get
+            {
+                return FPrice * FCount; // 總價格 = 單價 * 數量
+            }
+        }
+    }
+}
