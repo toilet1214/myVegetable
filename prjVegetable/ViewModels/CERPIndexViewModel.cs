c3be8bfde9922c0327ecc44630f87bf60cb7d815
CYan611 <ichang89611@gmail.com>
2025-02-02 16:35:47 +0800

    ERP首頁ViewModel

--- prjVegetable/ViewModels/CERPIndexViewModel.cs
+++ prjVegetable/ViewModels/CERPIndexViewModel.cs
@@ -1,6 +1,23 @@
-﻿namespace prjVegetable.ViewModels
+﻿using prjVegetable.Models;
+
+namespace prjVegetable.ViewModels
 {
     public class CERPIndexViewModel
     {
+        public int TotalMembers { get; set; }
+        public int TotalVisitors { get; set; }
+        public int TotalOrdersYear { get; set; }
+        public string BestSellingProductYear { get; set; }
+        public string MostPopularProductYear { get; set; }
+        public string BestSellingClassYear { get; set; }
+        public int TotalOrdersMonth { get; set; }
+        public string BestSellingProductMonth { get; set; }
+        public string MostPopularProductMonth { get; set; }
+        public string BestSellingClassMonth { get; set; }
+        public int TotalOrdersWeek { get; set; }
+        public string BestSellingProductWeek { get; set; }
+        public string MostPopularProductWeek { get; set; }
+        public string BestSellingClassWeek { get; set; }
+
     }
 }
