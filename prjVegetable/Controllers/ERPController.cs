db9a60be7fbac03ad4551530503df1ef78ff9b8e
CYan611 <ichang89611@gmail.com>
2025-02-05 22:45:50 +0800

    小修正

--- prjVegetable/Controllers/ERPController.cs
+++ prjVegetable/Controllers/ERPController.cs
@@ -220,7 +220,7 @@ namespace prjVegetable.Controllers
                     Count = o.ol.FCount,
                     State = order.FStatus
                 })
-                .Where(t => t.State == 2)
+                .Where(t => t.State == 1)
                 .GroupBy(x => x.Classification)
                 .Select(g => g.Sum(x => x.Count))
                 .ToList();
