0cfd309018c1be597483fb1156dbc4dfbec00b65
Noa77 <afra71593@gmail.com>
2025-02-07 16:23:00 +0800

    新建：首頁卡片管理 controller與View 更新：盤點單與調整單視圖 更新：首頁公告內容與排版

---
+++ prjVegetable/Controllers/IndexCardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjVegetable.Controllers
+{
+    public class IndexCardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
