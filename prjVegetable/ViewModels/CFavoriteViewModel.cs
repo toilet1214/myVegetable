205c18a9a79cb237c403f629060e28a314ca64a5
CYan611 <ichang89611@gmail.com>
2025-02-11 23:41:05 +0800

    我的最愛

---
+++ prjVegetable/ViewModels/CFavoriteViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace prjVegetable.ViewModels
+{
+    public class CFavoriteViewModel
+    {
+        public string Name { get; set; }
+        public int Popular {  get; set; }
+        public int FId { get; set; }
+    }
+}
