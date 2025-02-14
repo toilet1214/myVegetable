bb05d91eaa5058d2265f948b854e06447d45534a
Bertram Zhou <ghost.chou.1021@gmail.com>
2025-02-14 09:18:56 +0800

    小更新

---
+++ prjVegetable/ViewModels/CGoodsInAndOutViewModel.cs
@@ -0,0 +1,11 @@
+﻿using prjVegetable.Models;
+using prjVegetable.ViewModels;
+
+namespace prjVegetable.ViewModels
+{
+    public class CGoodsInAndOutViewModel
+    {
+        public TGoodsInAndOut GoodsInAndOut { get; set; } = new TGoodsInAndOut();
+        public List<TGoodsInAndOutDetail> GoodsInAndOutDetails { get; set; } = new List<TGoodsInAndOutDetail>();
+    }
+}
