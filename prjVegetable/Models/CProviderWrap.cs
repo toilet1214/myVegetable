a4cab510393edcc060bc331056e3d511174d17ee
Su <swimming.free.fish@gmail.com>
2025-02-27 14:32:51 +0800

    person index拿掉密碼欄位、provider顯示名稱修改

--- prjVegetable/Models/CProviderWrap.cs
+++ prjVegetable/Models/CProviderWrap.cs
@@ -23,7 +23,7 @@ namespace prjVegetable.Models
             set { _provider.FId = value; }
         }
 
-        [DisplayName("廠商名稱")]
+        [DisplayName("名稱")]
         [Required(ErrorMessage = "必填欄位")]
         public string FName
         {
@@ -31,7 +31,7 @@ namespace prjVegetable.Models
             set { _provider.FName = value; }
         }
 
-        [DisplayName("廠商統編")]
+        [DisplayName("統編")]
         [Required(ErrorMessage = "必填欄位")]
         [RegularExpression(@"^\d{8}$", ErrorMessage = "統一編號必須是 8 位數字")]
         public string FUbn//統編
@@ -40,7 +40,7 @@ namespace prjVegetable.Models
             set { _provider.FUbn = value; }
         }
 
-        [DisplayName("廠商電話")]
+        [DisplayName("電話")]
         [Required(ErrorMessage = "必填欄位")]
         [RegularExpression(@"^\d{8,10}$", ErrorMessage = "電話號碼必須是8到10位數字")]
         public string FTel
@@ -49,21 +49,21 @@ namespace prjVegetable.Models
             set { _provider.FTel = value; }
         }
 
-        [DisplayName("廠商聯絡人")]
+        [DisplayName("聯絡人")]
         public string FConnect
         {
             get { return _provider.FConnect; }
             set { _provider.FConnect = value; }
         }
 
-        [DisplayName("廠商會計")]
+        [DisplayName("會計")]
         public string FAccountant
         {
             get { return _provider.FAccountant; }
             set { _provider.FAccountant = value; }
         }
 
-        [DisplayName("廠商地址")]
+        [DisplayName("地址")]
         [Required(ErrorMessage = "必填欄位")]
         public string FAddress
         {
@@ -71,7 +71,7 @@ namespace prjVegetable.Models
             set { _provider.FAddress = value; }
         }
 
-        [DisplayName("廠商送貨地址")]
+        [DisplayName("送貨地址")]
         public string FDelivery
         {
             get { return _provider.FDelivery; }
@@ -87,7 +87,7 @@ namespace prjVegetable.Models
             }
         }
 
-        [DisplayName("廠商統編地址")]
+        [DisplayName("統編地址")]
         public string FInvoiceAdd
         {
             get { return _provider.FInvoiceAdd; }
